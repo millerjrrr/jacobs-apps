1281831a85df434439accc189053d971e1285ea9
Jacob <millerjr@tcd.ie>
2025-10-10 09:46:57 -0300

    updated griddier range builder to accept wizard ranges

--- src/griddier-range-builder/index.tsx
+++ src/griddier-range-builder/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HandEditor from "./components/HandEditor"; // <-- import the component
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "./store";
@@ -6,6 +5,10 @@ import { handsArray } from "./types"; // make sure you import your hands array
 import { resetRange, setHandAction } from "./store/rangeSlice";
 import FrequencyBar from "./components/FrequencyBar";
 import colors from "./utils/colors";
+import {
+  createRangeFromWizard,
+  isWizardRange,
+} from "./utils/createRangeFromWizard";
 
 function GriddierRangeBuilder() {
   const hands = useSelector((state: RootState) => state.range.hands);
@@ -62,7 +65,6 @@ function GriddierRangeBuilder() {
     try {
       const text = await navigator.clipboard.readText();
       if (!text) return alert("Clipboard is empty");
-
       const lines = text.trim().split("\n");
       // Ignore header
       const dataLines = lines.slice(1);
@@ -80,7 +82,46 @@ function GriddierRangeBuilder() {
 
         dispatch(setHandAction({ hand: hand as any, handActions }));
       });
+      setTimeout(() => alert("Range imported successfully!"), 1000);
+    } catch (err) {
+      console.error("Failed to import range: ", err);
+      alert("Failed to import range. Make sure clipboard format is correct.");
+    }
+  };
 
+  const handlePastePartialRange = async (
+    action: "allin" | "raise" | "call" | "prior"
+  ) => {
+    const proceed = window.confirm(
+      "Importing a range will overwrite your current range. Do you want to continue?"
+    );
+    if (!proceed) return;
+
+    try {
+      const text = await navigator.clipboard.readText();
+      if (!text) return alert("Clipboard is empty");
+      if (!isWizardRange(text)) return alert("Format is incorrect");
+
+      const wizData = createRangeFromWizard(text);
+
+      await Object.keys(wizData).forEach((wizHand) => {
+        if (!handsArray.includes(wizHand)) return;
+
+        const handActions = {
+          allin: Number(hands[wizHand].allin) as 0 | 1 | 2 | 3 | 4,
+          raise: Number(hands[wizHand].raise) as 0 | 1 | 2 | 3 | 4,
+          call: Number(hands[wizHand].call) as 0 | 1 | 2 | 3 | 4,
+          prior: Number(hands[wizHand].prior) as 0 | 1 | 2 | 3 | 4,
+          [action]: Number(Math.ceil(wizData[wizHand] * 4)) as
+            | 0
+            | 1
+            | 2
+            | 3
+            | 4,
+        };
+
+        dispatch(setHandAction({ hand: wizHand as any, handActions }));
+      });
       setTimeout(() => alert("Range imported successfully!"), 1000);
     } catch (err) {
       console.error("Failed to import range: ", err);
@@ -128,7 +169,7 @@ function GriddierRangeBuilder() {
               style={{
                 padding: "0.5rem 1rem",
                 cursor: "pointer",
-                width: 150,
+                width: 166,
               }}
             >
               Copy Range
@@ -138,7 +179,7 @@ function GriddierRangeBuilder() {
               style={{
                 padding: "0.5rem 1rem",
                 cursor: "pointer",
-                width: 150,
+                width: 166,
               }}
             >
               Paste Range
@@ -148,10 +189,60 @@ function GriddierRangeBuilder() {
               style={{
                 padding: "0.5rem 1rem",
                 cursor: "pointer",
-                width: 150,
+                width: 166,
               }}
             >
               Reset Range
+            </button>
+          </div>
+          <div
+            style={{
+              marginTop: "0.2rem",
+              display: "flex",
+              flexDirection: "row",
+              alignItems: "center",
+              gap: "0.5rem",
+            }}
+          >
+            <button
+              onClick={() => handlePastePartialRange("allin")}
+              style={{
+                padding: "0.5rem 1rem",
+                cursor: "pointer",
+                width: 125,
+              }}
+            >
+              Paste AllIn
+            </button>
+            <button
+              onClick={() => handlePastePartialRange("raise")}
+              style={{
+                padding: "0.5rem 1rem",
+                cursor: "pointer",
+                width: 125,
+              }}
+            >
+              Paste Raise
+            </button>
+            <button
+              onClick={() => handlePastePartialRange("call")}
+              style={{
+                padding: "0.5rem 1rem",
+                cursor: "pointer",
+                width: 125,
+              }}
+            >
+              Paste Call
+            </button>
+            <button
+              onClick={() => handlePastePartialRange("prior")}
+              style={{
+                padding: "0.5rem 1rem",
+                cursor: "pointer",
+                width: 125,
+              }}
+            >
+              Paste Prior
             </button>
           </div>
         </div>
