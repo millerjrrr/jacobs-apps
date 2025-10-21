0ab36ceb916168ae55e79da3b562d2f12c65610c
Jacob <millerjr@tcd.ie>
2025-10-21 11:49:07 -0300

    Updates

---
+++ link-king-com/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>https://link-king.com/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="https://link-king.com/xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="https://link-king.com/xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://link-king.com/xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="https://link-king.com/xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="https://link-king.com/wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
